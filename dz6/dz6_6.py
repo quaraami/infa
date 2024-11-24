c07a7a7fd684e28f1928cc047c861a97367f13b9
quaraami <shum05052000@gmail.com>
2024-11-24 14:27:35 +0300

    commit

--- dz6/dz6_6.py
+++ dz6/dz6_6.py
@@ -1,22 +1,24 @@
-#n = int(input()) #количество строк
-#m = int(input()) #количество столбцов
-#arr = list()
-#for i in range(n):
-  #brr = list()
-  #for i in range(m):
-    #brr.append(float(input()))
-  #arr.append(brr) #заполняю матрицу
-#for i in arr:
-   #print(' '.join(list(map(str, i))))
-n=4
-arr = [[5,2,17],
-        [10,3,9], 
-        [9,4,13], 
-        [4,5,9]]
+n = int(input("Введите количество строк:")) #количество строк
+m = int(input("Введите количество столбцов:")) #количество столбцов
+arr = list()
 for i in range(n):
-     print(arr[i].index(min(arr[i])))
-     print(f'в строке {i}')
-     print(f'минимальное значение в {i} строке {min(arr[i])}')
-    
+  brr = list()
+  for i in range(m):
+    brr.append(int(input()))
+  arr.append(brr) #заполняю матрицу
+print("Исходная матрица:")
+for i in arr:
+   print(' '.join(list(map(str, i)))) #вывожу матрицу
+for i in range(n):
+    h = arr[i].index(min(arr[i])) #задаю переменную которая при каждом 
+    #цикле меняется на индекс минимального числа в каждой строчке
+    for j in range(m):
+      if arr[i][h] % 2 == 0: #проверяю это число на четность
+            arr[i][h] = 0 
+      else:
+          arr[i][h] = 1
+print("Новая матрица:")
+for i in arr:
+   print(' '.join(list(map(str, i)))) #вывожу новую матрицу
 
 
